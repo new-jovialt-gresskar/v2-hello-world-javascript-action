2a3c93edf28688251fc8da112f328ee33d3b6467
new-jovialt-gresskar <148625881+new-jovialt-gresskar@users.noreply.github.com>
2023-10-21 16:52:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello hello, ${nameToGreet}!`);
+  console.log("This is a test!")
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 } catch (error) {
