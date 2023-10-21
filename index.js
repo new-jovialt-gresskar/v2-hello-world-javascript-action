d28d5e8b67117ca9aa3b14640253b34dee5779cc
Sindre Holtskog <sin-holt@online.no>
2023-10-21 16:47:22 +0200

    Add time

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello hello, ${nameToGreet}!`);
-  const time = "Hello, hello, hello"; //(new Date()).toTimeString();
+  const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 } catch (error) {
   core.setFailed(error.message);
